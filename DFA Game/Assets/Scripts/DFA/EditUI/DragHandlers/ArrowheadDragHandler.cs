9f25eee1e88e003b183b8678121fc45eba2236d0
hxhdrn <kobisaha@gmail.com>
2025-06-25 16:59:41 -0600

    comments

--- DFA Game/Assets/Scripts/DFA/EditUI/DragHandlers/ArrowheadDragHandler.cs
+++ DFA Game/Assets/Scripts/DFA/EditUI/DragHandlers/ArrowheadDragHandler.cs
@@ -1,19 +1,33 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/// <summary>
+/// ArrowheadDragHandler is a behavior that handles dragging a transition's arrowhead
+/// </summary>
 public class ArrowheadDragHandler : DragHandler
 {
+    /// <summary>Arrowline associated with the arrowhead</summary>
     [SerializeField] private DFAArrowline arrowline;
+    /// <summary>HoverDetector that detects a pointer hovering on the arrowhead</summary>
     [SerializeField] private HoverDetector hoverDetector;
+    /// <summary>HoverDetector that detects a pointer hovering on the arrowline</summary>
     [SerializeField] private HoverDetector lineHoverDetector;
+    /// <summary>Transition the arrowhead belongs to</summary>
     [SerializeField] private DFATransition transition;
 
+    /// <summary>
+    /// Disables hovering on the arrowhead and the arrowline
+    /// </summary>
     public override void StartBehavior()
     {
         hoverDetector.DisableHover();
         lineHoverDetector.DisableHover();
     }
 
+    /// <summary>
+    /// Sets new end state of the transition
+    /// Re-enabled hovering on the arrowhead and the arrowline
+    /// </summary>
     public override void StopBehavior()
     {
         if (HoverManager.Instance.CurrentBehavior is StateHoverHandler stateHover)
@@ -29,6 +43,9 @@ public class ArrowheadDragHandler : DragHandler
         // arrowline.UpdateStatePositions();
     }
 
+    /// <summary>
+    /// Updates the arrowhead and arrowline based on the current mouse position
+    /// </summary>
     public override void UpdateBehavior()
     {
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Pointer.current.position.ReadValue());
--- DFA Game/Assets/Scripts/Level Navigation/LevelButton.cs
+++ DFA Game/Assets/Scripts/Level Navigation/LevelButton.cs
@@ -3,14 +3,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+/// <summary>
+/// LevelButton represents one level in the level selection list that players can click on to enter the level
+/// </summary>
 public class LevelButton : MonoBehaviour
 {
     private string puzzleID;
+    /// <summary> ID of the puzzle this LevelButton opens /// </summary>
     public string PuzzleID { get => puzzleID; set { puzzleID = value; UpdateButton(); } }
     [SerializeField] private Button button;
     [SerializeField] private TextMeshProUGUI buttonText;
     [SerializeField] private Image doneImage;
-    
+
+    /// <summary>Updates the button text and enables/disables the button based on if the puzzle is unlocked using the current PuzzleID</summary>
     public void UpdateButton()
     {
         PuzzleData.Puzzle puzzle = PuzzleData.Instance.GetPuzzleData(PuzzleID);
@@ -19,6 +24,7 @@ public class LevelButton : MonoBehaviour
         doneImage.gameObject.SetActive(puzzle.completed);
     }
 
+    /// <summary>Opens the puzzle level this button points to</summary>
     public void PlayPuzzle()
     {
         PuzzleSelectionManager.Instance.PuzzleID = PuzzleID;
